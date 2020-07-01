047d65a965ac4b26dd225c30b9e3af7caa504c0b
Clare Jung <clare.jung@hotmail.com>
2020-07-01 11:47:43 +0900

    css update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const emojis = document.querySelectorAll('#emoji');
 emojis.forEach(emoji => emoji.addEventListener('mouseover', function(e){
-    emoji.classList.add('animated','bounce')
+    emoji.classList.add('animated','shake')
 }))
 
 emojis.forEach(emoji => emoji.addEventListener('mouseout', function(e){
+    emoji.classList.remove('animated','shake')
+}))
+
+const emojis2 = document.querySelectorAll('#emoji2');
+emojis2.forEach(emoji => emoji.addEventListener('mouseover', function(e){
+    emoji.classList.add('animated','bounce')
+}))
+
+emojis2.forEach(emoji => emoji.addEventListener('mouseout', function(e){
     emoji.classList.remove('animated','bounce')
 }))
