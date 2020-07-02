e6ba7bc3639d7a07e41035b2f72b3c1f73a9cf2a
Clare Jung <clare.jung@hotmail.com>
2020-07-02 23:00:28 +0900

    modal

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ emojis2.forEach(emoji => emoji.addEventListener('mouseover', function(e){
 emojis2.forEach(emoji => emoji.addEventListener('mouseout', function(e){
     emoji.classList.remove('animated','bounce')
 }))
+
+var btn = document.getElementsByClassName("click-to-open");
+
+for (var i = 0; i < btn.length; i++) {
+  var thisBtn = btn[i];
+  thisBtn.addEventListener("click", function(){
+    var modal = document.getElementById(this.dataset.modal);
+    modal.style.display = "flex";
+}, false)};
+
+document.querySelector('.close-button').addEventListener('click',
+function() {
+    document.querySelector('.click-to-open').style.display = 'none';
+});
